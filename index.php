a64865f371af903fc73bb5772ec058bd4d6a164a
dorincop <101493930+dorincop@users.noreply.github.com>
2022-07-01 18:05:14 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
-<title><?php echo $_GET['a'];
-echo ' - 그냥위키';?> </title>
 <?php
-// 라이브러리를 불러옵니다.
 require_once("namumark.php");
-?>
-<font size=14><?php
-$sdf = $_GET['a'];
-echo $sdf;?>
-</font>
-<?php
-// MySQLWikiPage와는 달리 PlainWikiPage의 첫 번째 인수로 위키텍스트를 받습니다.
-$a = array('나무마크' => '[목차]
-== 개요 ==
-위키 문법이다.');
-$wPage = new PlainWikiPage($a[$sdf]);
-// NamuMark 생성자는 WikiPage를 인수로 받습니다.
+$wPage = new PlainWikiPage("위키텍스트");
 $wEngine = new NamuMark($wPage);
+$wEngine->prefix = "/wiki";
+echo $wEngine->toHtml();
+session_start();
+if(!isset($_SESSION['username'])) {
+    echo "<script>location.replace('login.php');</script>";            
+}
 
-// 위키링크의 앞에 붙을 경로를 prefix에 넣습니다.
-$wEnginee = $wEngine->toHtml();
-
-// toHtml을 호출하면 HTML 페이지가 생성됩니다.
-echo $wEnginee;
+else {
+    $username = $_SESSION['username'];
+    $name = $_SESSION['name'];
+} 
+?>
+<body>
+    <div class="base">
+        <h2><?php echo "안녕하세요, $username($name)님";?></h2>
+        <button type="button" class="btn" onclick="location.href='logout.php'">
+            LOGOUT
+        </button>
+    </div>
+</body>
